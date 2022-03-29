603f69de3c40f591e9e9d95f7ce27da18d844650
Jason <ya850805@gmail.com>
2022-03-29 23:39:11 +0800

    feat: Initial commit.

---
+++ src/main/java/com/crypto/CryptoSoftwareEngineerExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoSoftwareEngineerExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoSoftwareEngineerExerciseApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/crypto/service/TradeService.java
@@ -0,0 +1,4 @@
+package com.crypto.service;
+
+public interface TradeService {
+}
